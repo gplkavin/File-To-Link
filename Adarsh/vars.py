7a5e5afb3a50e5b359d489ead8438f1f43f28bfc
gplkavin <81810902+gplkavin@users.noreply.github.com>
2022-10-05 12:02:36 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -17,7 +17,7 @@ class Var(object):
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(environ.get("BIN_CHANNEL", "-1001159368239"))
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = "172.31.89.213"
+    BIND_ADRESS = "54.69.246.204"
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1174794359").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
@@ -29,7 +29,7 @@ class Var(object):
     
     else:
         ON_HEROKU = False
-    FQDN = "172.31.89.213"
+    FQDN = "54.69.246.204"
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
